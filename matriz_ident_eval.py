2a7eb1cb327cd43bcec7bcdfe057d8dd95dfa87c
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-03-24 19:48:47 -0400

    Create matriz_ident_eval.py

---
+++ matriz_ident_eval.py
@@ -0,0 +1,14 @@
+def identidade(matriz): 
+  for i in range (len(matriz)): 
+  for j in range (len(matriz[0])): 
+    if i == j and matriz[i][j] != 1: 
+      return False 
+    if i != j and matriz[i][j] != 0: 
+      return False 
+  return True 
+
+matriz = eval(input()) 
+  if identidade(matriz): 
+    print('sim') 
+  else: 
+    print('nao')
